85e52d9071cc70d2640f5bc76340539ee82a88c1
Windows <massiopia@gmail.com>
2021-03-27 02:04:20 -0700

    edit user endpoint and checkauth middleware done

--- server/middleware/checkauth.js
+++ server/middleware/checkauth.js
@@ -1,3 +1,21 @@
+const axios = require('axios');
+
 module.exports = (req, res, next) => {
-    //insert discord logic here
+    if(!req.headers.authorization) return res.json({error: "No authorization!"}).status(401).end()
+
+    axios.get("https://discord.com/api/v8/users/@me", {
+        headers: {
+            authorization: `Bearer ${req.headers.authorization}`
+        }
+    }).then((res_) => {
+        if(res_.data){
+            req.user = res_.data;
+            next();
+        } else {
+            res_.json({error: "Internal server error!"}).status(500);
+        }
+    }).catch((err) => {
+        console.log(err);
+        res.json({error: "Bad auth!"}).status(403);
+    });
 };
--- server/routes/api/v1/users.js
+++ server/routes/api/v1/users.js
@@ -1,8 +1,18 @@
 const {Router} = require('express');
 const Users = require('../../../models/user');
+const checkauth = require('../../../middleware/checkauth');
 
 let route = Router();
 
+
+route.post("/:id/edit", checkauth, async (req, res, next) => {
+    let user = await Users.findOne({user_id: req.params.id});
+
+    if(!user) return res.json({error: "Cannot find user!"});
+
+    if(req.user.id !== user.user_id) return res.json({error: "You are not permitted to edit this user!"})
+
+});
 
 route.get("/:id", async (req, res, next) => {
     let user = await Users.findOne({user_id: req.params.id});
