70c19b3bd2af27f7c4994f69944713168ab9c798
MattA-Official <matthew.n.adcock@gmail.com>
2021-04-29 20:43:04 +0100

    Updated Form Validation
    - You can now press the back button without filling the application form in
    - All incorrect fields are now highlighted

--- dmod/components/ApplySteps/About.tsx
+++ dmod/components/ApplySteps/About.tsx
@@ -9,60 +9,64 @@ import DatePicker from '../Forms/date_picker';
 
 function About(props) {
 
-    let [data, setData] = useState({});
-    let [focusedCalander, setFocused] = useState(false);
-    let [date, setDate] = useState()
-    let [aboutUnfilled, setAboutUnfilled] = useState(true);
-    let [modUnfilled, setModUnfilled] = useState(true);
-    let [genderUnfilled, setGenderUnfilled] = useState(true);
-    let [birthdayUnfilled, setBirthdayUnfilled] = useState(true);
+	let [data, setData] = useState({});
+	let [focusedCalander, setFocused] = useState(false);
+	let [date, setDate] = useState()
+	let [aboutUnfilled, setAboutUnfilled] = useState(true);
+	let [modUnfilled, setModUnfilled] = useState(true);
+	let [genderUnfilled, setGenderUnfilled] = useState(true);
+	let [birthdayUnfilled, setBirthdayUnfilled] = useState(true);
 
-    let onFormSubmit = (e) => {
-        e.preventDefault();
+	let onFormSubmit = (e) => {
+		e.preventDefault();
 
-        let submitter = e.nativeEvent.submitter.name;
-        console.log(data);
-        //doing this so that if the user clicks next it showes the red around the box not when they first get to this page 
-        if(!data.about) return setData({ ...data, about: ""});
-        if(!data.description) return setData({...data, description: ""});
-        if(!data.pronouns) return setData({...data, pronouns: ""});
-        if(!data.birthday) return setData({...data, birthday: ""});
+		let submitter = e.nativeEvent.submitter.name;
+		console.log(data);
+		//doing this so that if the user clicks next it showes the red around the box not when they first get to this page 
+		if (submitter === "forward") {
+			if (!data.about) setData({ ...data, about: "" });
+			if (!data.description) setData({ ...data, description: "" });
+			if (!data.pronouns) setData({ ...data, pronouns: "" });
+			if (!data.birthday) setData({ ...data, birthday: "" });
+			if (!data.about || !data.description || !data.pronouns || !data.birthday) return
+		}
 
-        if (props.onSubmit) {
-            props.onSubmit(submitter, data);
-        }
-    }
+		if (props.onSubmit) {
+			props.onSubmit(submitter, data);
+		}
+	}
 
-    let onAboutChange = (d) => {
-        setData({ ...data, about: d })
-    }
+	let onAboutChange = (d) => {
+		setData({ ...data, about: d })
+	}
 
-    let onModChange = (d) => {
-        setData({ ...data, description: d })
-    }
+	let onModChange = (d) => {
+		console.log(d)
+		setData({ ...data, description: d })
+	}
 
-    let onSelectorChange = (d) => {
-        setData({ ...data, pronouns: d })
-    }
+	let onSelectorChange = (d) => {
+		setData({ ...data, pronouns: d })
+	}
 
-    let onDateChange = (d) => {
-        setData({...data, birthday: d});
-    }
+	let onDateChange = (d) => {
+		setData({ ...data, birthday: d });
+	}
 
-    return (
-        <div className={styles.fade_in_container}>
-            <form onSubmit={onFormSubmit}>
-                <FormField onChange={onAboutChange} filled={data.about} image={"/pencil.png"} placeholder={"Write your about text here"} name={"About"}></FormField>
-                <FormField onChange={onModChange} filled={data.description} image={"/pencil.png"} placeholder={"Write your text here"} name={"Why do you want to be a mod?"}></FormField>
-                <Selector onChange={onSelectorChange} filled={data.pronouns} image={"gender.png"} name={"Pronouns (We also support pronoundb)"} items={["He/Him", "She/Her", "They/Them", "Other"]}></Selector>
-                <DatePicker filled={data.birthday} onChange={onDateChange}></DatePicker>
-                <div className={styles.button_container_form}>
-                    <button className={`${Buttons.small_button} ${Buttons.border} ${forms.button_spacer}`} name={"back"} >Go Back</button>
-                    <button className={`${Buttons.small_button} ${Buttons.colored}`} name={"forward"}>Next</button>
-                </div>
-            </form>
-        </div>
-    );
+	return (
+		<div className={styles.fade_in_container}>
+			<form onSubmit={onFormSubmit}>
+				<FormField onChange={onAboutChange} filled={data.about} image={"/pencil.png"} placeholder={"Write your about text here"} name={"About"}></FormField>
+				<FormField onChange={onModChange} filled={data.description} image={"/pencil.png"} placeholder={"Write your text here"} name={"Why do you want to be a mod?"}></FormField>
+				<Selector onChange={onSelectorChange} filled={data.pronouns} image={"gender.png"} name={"Pronouns (We also support pronoundb)"} items={["He/Him", "She/Her", "They/Them", "Other"]}></Selector>
+				<DatePicker filled={data.birthday} onChange={onDateChange}></DatePicker>
+				<div className={styles.button_container_form}>
+					<button className={`${Buttons.small_button} ${Buttons.border} ${forms.button_spacer}`} name={"back"} >Go Back</button>
+					<button className={`${Buttons.small_button} ${Buttons.colored}`} name={"forward"}>Next</button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 
