7276e9665d49e57dbc34d49d6acc9ee43a2f7ae9
MattA-Official <matthew.n.adcock@gmail.com>
2021-05-16 22:01:25 +0100

    Validation with react-hook-form

--- dmod/components/ApplySteps/About.tsx
+++ dmod/components/ApplySteps/About.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import { useState } from 'react';
+import { useForm, useFormState } from 'react-hook-form';
 import styles from '../../styles/apply.module.scss';
 import Buttons from '../../styles/buttons.module.scss';
 import forms from '../../styles/forms.module.scss';
@@ -9,57 +10,38 @@ import DatePicker from '../Forms/date_picker';
 
 function About(props) {
 
-	let [data, setData] = useState({});
-	let [focusedCalander, setFocused] = useState(false);
-	let [date, setDate] = useState()
-	let [aboutUnfilled, setAboutUnfilled] = useState(true);
-	let [modUnfilled, setModUnfilled] = useState(true);
-	let [genderUnfilled, setGenderUnfilled] = useState(true);
-	let [birthdayUnfilled, setBirthdayUnfilled] = useState(true);
+	let [data, setData] = useState({})
 
-	let onFormSubmit = (e) => {
-		e.preventDefault();
+	const { register, handleSubmit, reset, watch, formState: { errors }, control } = useForm({ mode: 'onTouched' });
 
-		let submitter = e.nativeEvent.submitter.name;
-		console.log(data);
-		//doing this so that if the user clicks next it showes the red around the box not when they first get to this page 
-		if (submitter === "forward") {
-			if (!data.about) setData({ ...data, about: "" });
-			if (!data.description) setData({ ...data, description: "" });
-			if (!data.pronouns) setData({ ...data, pronouns: "" });
-			if (!data.birthday) setData({ ...data, birthday: "" });
-			if (!data.about || !data.description || !data.pronouns || !data.birthday) return
-		}
+	const { isValid, isDirty } = useFormState({ control });
 
-		if (props.onSubmit) {
-			props.onSubmit(submitter, data);
-		}
-	}
+	const updateData = async ({ about, description, pronouns, birthday }) => {
 
-	let onAboutChange = (d) => {
-		setData({ ...data, about: d })
-	}
+		console.log(data)
 
-	let onModChange = (d) => {
-		console.log(d)
-		setData({ ...data, description: d })
-	}
+		setData({ ...data, about, description, pronouns, birthday })
+		reset({ about, description, pronouns, birthday });
 
-	let onSelectorChange = (d) => {
-		setData({ ...data, pronouns: d })
-	}
-
-	let onDateChange = (d) => {
-		setData({ ...data, birthday: d });
-	}
+	};
 
 	return (
 		<div className={styles.fade_in_container}>
-			<form onSubmit={onFormSubmit}>
-				<FormField onChange={onAboutChange} filled={data.about} image={"/pencil.png"} placeholder={"Write your about text here"} name={"About"}></FormField>
-				<FormField onChange={onModChange} filled={data.description} image={"/pencil.png"} placeholder={"Write your text here"} name={"Why do you want to be a mod?"}></FormField>
-				<Selector onChange={onSelectorChange} filled={data.pronouns} image={"gender.png"} name={"Pronouns (We also support pronoundb)"} items={["He/Him", "She/Her", "They/Them", "Other"]}></Selector>
-				<DatePicker filled={data.birthday} onChange={onDateChange}></DatePicker>
+			<form onSubmit={handleSubmit(updateData)}>
+				<FormField image={"/pencil.png"} placeholder={"Write your about text here"} name={"About"} error={errors.about} register={{
+					...register("about", {
+						maxLength: 20,
+						required: true,
+					})
+				}} />
+				<FormField image={"/pencil.png"} placeholder={"Write your text here"} name={"Why do you want to be a mod?"} error={errors.description} register={{
+					...register("description", {
+						maxLength: 20,
+						required: true,
+					})
+				}} />
+				<Selector image={"/gender.png"} name={"Pronouns (We also support pronoundb)"} items={["He/Him", "She/Her", "They/Them", "Other"]} />
+				<DatePicker></DatePicker>
 				<div className={styles.button_container_form}>
 					<button className={`${Buttons.small_button} ${Buttons.border} ${forms.button_spacer}`} name={"back"} >Go Back</button>
 					<button className={`${Buttons.small_button} ${Buttons.colored}`} name={"forward"}>Next</button>
