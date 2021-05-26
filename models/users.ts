b05175a6bcb6ce52730b2d65409a9ff651610147
sayantan300 <rovelstars300@gmail.com>
2021-05-26 22:31:50 +0530

    hmm (this is my style of commit msgs)

--- models/users.ts
+++ models/users.ts
@@ -15,7 +15,7 @@ const mongoose = require('mongoose');
    from: {type: Date, required: true},
    to: {type: Date, required: true}
   },
-  tz:{type: String, required: true},
+  tz:String,
   ratings: [{
    _id: {
     default: () => new Date(),
