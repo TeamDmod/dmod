098ff346384ff9e7c2ffc90bfc79aefc54b28fd2
Matthew Adcock <matthew.n.adcock@gmail.com>
2021-05-29 16:57:19 +0100

    Added Prettier for formatting. Updated Home & 404

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import '../styles/globals.scss';
-import '../styles/navbar.scss';
-import Navbar from '../components/Navbar';
+import "../styles/globals.scss";
+import "../styles/navbar.scss";
+import Navbar from "../components/Navbar";
 import "../styles/calender.scss";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-        <Navbar />
-        <Component {...pageProps} />
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<Component {...pageProps} />
+		</>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,32 @@
-import home from '../styles/home.module.scss';
-import MetaTags from '../components/MetaTags';
+import home from "../styles/home.module.scss";
+import MetaTags from "../components/MetaTags";
 
 export default function Home() {
-  return (
-    <>
-      <MetaTags title={"Welcome to Dmod.gg"} description={"The best place to hire moderators!"} image={"/logo.png"} />
-      <div className={home.main_header}>
-      	<div className={home.text_headings}>
-          <h1>Welcome to dmod.</h1>
-          <p>Discords most advanced moderator searching application.</p>
-      	</div>
-      	<div className={home.search_container}>
-          <input placeholder="Search dmod." className={home.search_bar}></input>
-          <button className={home.search_button}>Search.</button>
-      	</div>
-      </div>
-      <div className={home.home_main_content}>
-        <div className={home.top_rated}>
-          <div className={home.heading}>
-            <h1>Top rated moderators.</h1>
-            <p>Based on user feedback these people are the best!</p>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<MetaTags
+				title={"Welcome to Dmod.gg"}
+				description={"The best place to hire moderators!"}
+				image={"/logo.png"}
+			/>
+			<div className={home.main_header}>
+				<div className={home.text_headings}>
+					<h1>Welcome to dmod.</h1>
+					<p>Discords most advanced moderator searching application.</p>
+				</div>
+				<div className={home.search_container}>
+					<input placeholder="Search dmod." className={home.search_bar}></input>
+					<button className={home.search_button}>Search.</button>
+				</div>
+			</div>
+			<div className={home.home_main_content}>
+				<div className={home.top_rated}>
+					<div className={home.heading}>
+						<h1>Top rated moderators.</h1>
+						<p>Based on user feedback these people are the best!</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
