322ec8e9fb859c1524a91681535537ce07ea832a
Zaid "Nico <me@zaid-arshad.tech>
2021-06-15 09:51:38 -0400

    chore: add eslint

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "printWidth": 110,
+    "tabWidth": 4,
+    "useTabs": false,
+    "semi": true,
+    "singleQuote": false,
+    "trailingComma": "none",
+    "bracketSpacing": true,
+    "jsxBracketSameLine": true,
+    "arrowParens": "always",
+    "requirePragma": false,
+    "insertPragma": false,
+    "proseWrap": "always"
+}
