ccf08248aee19ef8378f2099d95582860316c15c
Matthew Adcock <matthew.n.adcock@gmail.com>
2021-08-11 16:53:42 +0100

    Update MetaTags.tsx

--- components/MetaTags.tsx
+++ components/MetaTags.tsx
@@ -1,19 +1,28 @@
 import Head from 'next/head';
 
-// eslint-disable-next-line react/prop-types
-export default function Metatags({ title = 'dmod.gg', description = 'The best place to hire moderators!', image = '/logo.png' }) {
-  return (
-    <Head>
-      <title>{title}</title>
-      <meta name='twitter:card' content='summary' />
-      <meta name='twitter:site' content='@dmod.gg' />
-      <meta name='twitter:title' content={title} />
-      <meta name='twitter:description' content={description} />
-      <meta name='twitter:image' content={image} />
+export default function Metatags({
+	title = 'dmod.gg',
+	description = 'Innovating the Moderation Community. Moderator & Server Profiles, Built-in Applications for Owners, Moderator Listings, Resources, and so much more!',
+	image = '/logo-solid.png',
+	color = '#0B172A',
+}) {
+	return (
+		<Head>
+			<title>{title}</title>
 
-      <meta property='og:title' content={title} />
-      <meta property='og:description' content={description} />
-      <meta property='og:image' content={image} />
-    </Head>
-  );
+			<meta name='description' content={description} />
+			<meta name='image' content={image} />
+			<meta name='theme-color' content={color} />
+
+			<meta name='twitter:card' content='summary' />
+			<meta name='twitter:site' content='@dmod.gg' />
+			<meta name='twitter:title' content={title} />
+			<meta name='twitter:description' content={description} />
+			<meta name='twitter:image' content={image} />
+
+			<meta property='og:title' content={title} />
+			<meta property='og:description' content={description} />
+			<meta property='og:image' content={image} />
+		</Head>
+	);
 }
