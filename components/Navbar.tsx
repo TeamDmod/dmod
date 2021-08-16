bff0c1d559cce3157aade438b87acf2def0ab30e
Matthew Adcock <matthew.n.adcock@gmail.com>
2021-08-16 20:39:37 +0100

    Navbar + some of the search page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,12 +16,7 @@ function BurgerIcon() {
       stroke='currentColor'
       viewBox='0 0 24 24'
       xmlns='http://www.w3.org/2000/svg'>
-      <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        strokeWidth={2}
-        d='M4 6h16M4 12h16M4 18h16'
-      />
+      <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4 6h16M4 12h16M4 18h16' />
     </svg>
   );
 }
@@ -35,23 +30,12 @@ function CrossIcon() {
       stroke='currentColor'
       viewBox='0 0 24 24'
       xmlns='http://www.w3.org/2000/svg'>
-      <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        strokeWidth={2}
-        d='M4 4L18 18M18 4L4 18'
-      />
+      <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4 4L18 18M18 4L4 18' />
     </svg>
   );
 }
 
-export default function Navbar({
-  user,
-  fetcher,
-}: {
-  user: sessionFetchedUser;
-  fetcher: any;
-}) {
+export default function Navbar({ user, fetcher }: { user: sessionFetchedUser; fetcher: any }) {
   const isMobile = useMediaQuery({ maxWidth: 768 });
   const [active, setActive] = useState(false);
   const [winlogOpen, setWinlogOpen] = useState(false);
@@ -107,28 +91,16 @@ export default function Navbar({
     <header className={styles.navbar}>
       <nav>
         <Link href='/'>
-          <img
-            src='/logo.png'
-            alt='dmod.gg logo'
-            width={48}
-            height={48}
-            className='pointer'
-          />
+          <img src='/logo.png' alt='dmod.gg logo' width={48} height={48} className={styles.logo} />
         </Link>
         {isMobile &&
           ((!active && (
-            <button
-              onClick={() => setActive(!active)}
-              name='menu'
-              className={styles.menu}>
+            <button onClick={() => setActive(!active)} name='menu' className={styles.menu}>
               <BurgerIcon />
             </button>
           )) ||
             (active && (
-              <button
-                onClick={() => setActive(!active)}
-                name='menu'
-                className={styles.menu}>
+              <button onClick={() => setActive(!active)} name='menu' className={styles.menu}>
                 <CrossIcon />
               </button>
             )))}
